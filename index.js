a1567828d5e9a4c06e996c36a2107268a9bea2ac
tonynereus <86659901+tonynereus@users.noreply.github.com>
2024-01-24 03:18:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,36 +8,35 @@ app.use(cors());
 app.post("/api",(req,res)=>{
   
     var data = req.body;
-    console.log(data);
-    res.send(JSON.stringify({status:true,message:"seen"}));
-    res.end();
-    res.send()
-  //   var txt = myMail.myMail(data.eventData.venue,data.eventData.note);
-  //   var transporter = nodemailer.createTransport({
-  //   service: 'gmail',
-  //   auth: {
-  //     user: 'tonynereus@gmail.com',
-  //     pass: 'tghsvxyjqmdowzsu'
-  //   }
-  // });
-  // var mailOptions = {
-  //   from: 'tonynereus@gmail.com',
-  //   to: data.email,
-  //   subject: 'Taylor Swift Ticket | The Eras Tour',
-  // };
-  // if(data.email != undefined ){
-  //   transporter.sendMail({...mailOptions,html:txt}, function(error, info){
-  //       if (error) {
-  //         console.log(error);
-  //         res.send(JSON.stringify({status:false,message:"seen error with email"}))
-  //       }else {
-  //           res.send(JSON.stringify({status:true,message:"seen"}))
-  //         console.log('Email sent: ' + info.response);
-  //       }
-  //     });
-  // }else{
-  //   res.send(JSON.stringify({status:true,message:"email not sent "}))
-  // }
+    data = JSON.parse(data);
+    var txt = myMail.myMail(data.eventData.venue,data.eventData.note);
+    var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'tonynereus@gmail.com',
+      pass: 'tghsvxyjqmdowzsu'
+    }
+  });
+  var mailOptions = {
+    from: 'tonynereus@gmail.com',
+    to: data.email,
+    subject: 'Taylor Swift Ticket | The Eras Tour',
+  };
+  if(data.email != undefined ){
+    transporter.sendMail({...mailOptions,html:txt}, function(error, info){
+        if (error) {
+          console.log(error);
+          res.send(JSON.stringify({status:false,message:"seen error with email"}))
+        }else {
+            res.send(JSON.stringify({status:true,message:"seen"}))
+          console.log('Email sent: ' + info.response);
+        }
+        res.end()
+      });
+  }else{
+    res.send(JSON.stringify({status:true,message:"email not sent "}));
+    res.end()
+  }
 });
 const server = http.createServer(app);
 server.listen(3000)
